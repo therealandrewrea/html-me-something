89ea8ad45e9e2c90ca29cbfc94a5c6dfd52fff4b
Andy Rea <therealandrewrea@gmail.com>
2017-05-18 18:23:26 -0500

    Fragments in my templates

--- code/lc101/cheese-mvc/src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ code/lc101/cheese-mvc/src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by area on 5/17/17.
@@ -16,7 +18,10 @@ import java.util.ArrayList;
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    // static ArrayList<String> cheeses = new ArrayList<>(); //
+
+    static HashMap<String, String> cheeses = new HashMap<>();
+
 
     @RequestMapping(value = "")
     public String index (Model model) {
@@ -33,8 +38,8 @@ public class CheeseController {
     }
 
     @RequestMapping(value="add", method=RequestMethod.POST)
-    public String processAddCheeseForm(@ReqeustParam String cheeseName) {
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(@ReqeustParam String cheeseName, @RequestParam String cheeseDescription) {
+        cheeses.put(cheeseName, cheeseDescription);
         return "redirect:";
 
     }
